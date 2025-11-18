cc93cae09b8730fcc8f71bb388195dbe24d4e37f
Kotek2005 <151841030+Kotek2005@users.noreply.github.com>
2025-11-18 20:55:58 +0100

    punkto 1 tasko 3

--- PycharmProjects/HalfLifeThree/main.py
+++ PycharmProjects/HalfLifeThree/main.py
@@ -3,23 +3,136 @@ import numpy as np
 import sys
 
 def makeArray(image):
-    im = Image.open(image)
+    im = Image.open(image).convert('1')
     arr = np.array(im.getdata())
     arr = arr.reshape(im.size[1], im.size[0])
     return arr
 
-def makeImage(arr,name):
-    newIm = Image.fromarray(arr.astype(np.uint8))
+def makeImage(arr, name):
+
+    import numpy as np
+    from PIL import Image
+
+    arr_bool = arr > 0
+    newIm = Image.fromarray(arr_bool.astype(np.uint8) * 255).convert("1")
     newIm.save(name)
 
 def doDilation(filename):
-    print(f"Function doDilation invoked for {filename}")
-    arr = makeArray(filename)
+    if isinstance(filename, str):
+        print(f"Function doDilation invoked for {filename}")
+        arr = makeArray(filename)
+    else:
+        arr = filename
     arrnew = np.pad(arr, 1, "edge")
     SE = np.ones((3, 3), dtype=np.uint8)
+    SE = SE*255
     out = np.zeros_like(arr)
+    for i in range(1, arrnew.shape[0]-1):
+        for j in range(1, arrnew.shape[1]-1):
+            region = arrnew[i-1:i+2, j-1:j+2]
+            if np.any(region & SE):
+                out[i-1,j-1] = 255
+            else:
+                out[i-1,j-1] = 0
+    if isinstance(filename, str):
+        makeImage(out, f"dilated_{filename}")
+    return out
 
+#def doDilationTwo(arr,x,y):
+    arrnew = np.pad(arr, 1, "edge")
+    arrnew = arrnew > 0
+    SE = np.ones((3, 3), dtype=np.uint8)
+    out = np.zeros_like(arr)
+    for i in range(1, arrnew.shape[0]-1):
+        for j in range(1, arrnew.shape[1]-1):
+            region = arrnew[i-1:i+2, j-1:j+2]
+            if np.any(region & SE):
+                out[i-1,j-1] = 1
+            else:
+                out[i-1,j-1] = 0
 
+    return out
+
+def doErosion(filename):
+    if isinstance(filename, str):
+        print(f"Function doErosion invoked for {filename}")
+        arr = makeArray(filename)
+    else:
+        arr = filename
+    arrnew = np.pad(arr, 1, "edge")
+    SE = np.ones((3, 3), dtype=np.uint8)
+    SE = SE*255
+    out = np.zeros_like(arr)
+    for i in range(1, arrnew.shape[0]-1):
+        for j in range(1, arrnew.shape[1]-1):
+            region = arrnew[i-1:i+2, j-1:j+2]
+            if np.all(region & SE):
+                out[i-1,j-1] = 255
+            else:
+                out[i-1,j-1] = 0
+
+    if isinstance(filename, str):
+        makeImage(out, f"eroded_{filename}")
+    return out
+
+def doOpening(filename):
+    print(f"Function doOpening invoked for {filename}")
+    eroded = doErosion(filename)
+    out = doDilation(eroded)
+
+    makeImage(out, f"opened_{filename}")
+    return out
+
+def doClosing(filename):
+    print(f"Function doClosing invoked for {filename}")
+    dilated = doDilation(filename)
+    out = doErosion(dilated)
+
+    makeImage(out, f"closed_{filename}")
+    return out
+
+def doHitOrMiss(filename):
+    print(f"Function doHitOrMiss invoked for {filename}")
+    arr = makeArray(filename)
+    arrbool = arr > 0
+    arrnew = np.pad(arrbool, 1, "edge")
+    outh = np.zeros_like(arr)
+    outm = np.zeros_like(arr)
+    SEh = np.array([[1, 1, 1],
+                    [1, 1, 1],
+                    [1, 1, 0]], dtype=bool)
+
+    SEm = np.array([[0, 0, 0],
+                    [0, 0, 0],
+                    [0, 0, 1]], dtype=bool)
+
+    for i in range(1, arrnew.shape[0]-1):
+        for j in range(1, arrnew.shape[1]-1):
+            region = arrnew[i-1:i+2, j-1:j+2]
+            if np.all(region[SEh]):
+                outh[i-1,j-1] = 1
+            if np.all(~region[SEm]):
+                outm[i-1,j-1] = 1
+
+    out = outh & outm
+    outim = out*255
+    makeImage(outim, f"hit_or_miss_{filename}")
+    return out
+
+#def doEmThree(filename,x,y):
+    print(f"Function doM3 invoked for {filename}")
+    arr = makeArray(filename)
+    arrnew = arr > 0
+    arrnew[x,y] = True
+
+    while True:
+        arrold = arrnew.copy()
+        arrdil = doDilationTwo(arrnew,x,y)
+        arrnew = arrdil & (arr>0)
+        if np.array_equal(arrnew, arrold):
+            break
+
+    makeImage(arrnew, f"emThree_{filename}")
 
 
 
@@ -40,5 +153,16 @@ if len(sys.argv) > 3:
 if len(sys.argv) > 4:
     param2 = int(sys.argv[4])
 
-if command == '--dilation':
+if command == '--dil':
     doDilation(filenamen)
+elif command == '--ero':
+    doErosion(filenamen)
+elif command == '--open':
+    doOpening(filenamen)
+elif command == '--close':
+    doClosing(filenamen)
+elif command == '--hmt':
+    doHitOrMiss(filenamen)
+elif command == '--m3':
+    doEmThree(filenamen,int(param),int(param2))
+
